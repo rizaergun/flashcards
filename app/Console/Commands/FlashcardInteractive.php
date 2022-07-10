becca543fd6095744fa995e5cacd9dc29d24c950
Riza Ergun <re@rizaergun.com>
2022-07-10 16:43:43 +0300

    create flashcard:interactive command

---
+++ app/Console/Commands/FlashcardInteractive.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class FlashcardInteractive extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'flashcard:interactive';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Flashcard Interactive';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return 0;
+    }
+}
