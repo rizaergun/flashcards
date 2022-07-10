208107d4b3766c90f8d5f63fa10aedace57a8e36
Riza Ergun <re@rizaergun.com>
2022-07-10 17:16:56 +0300

    flashcard practice

--- app/Console/Commands/FlashcardInteractive.php
+++ app/Console/Commands/FlashcardInteractive.php
@@ -28,8 +28,9 @@ class FlashcardInteractive extends Command
      */
     public function handle()
     {
+        // Flashcard Menu
+        choice:
         $defaultIndex = "create";
-
         $index = $this->choice(
             'What do you want to do?',
             ['create', 'list', 'practice', 'stats', 'reset', 'exit'],
@@ -59,6 +60,56 @@ class FlashcardInteractive extends Command
                 ['Question', 'Answer'],
                 Flashcard::all(['question', 'answer'])->toArray()
             );
+        }
+
+        // Practice
+        practice:
+        if ($index === "practice") {
+            $flashcards = Flashcard::all(['id', 'question', 'user_answer']);
+            $count_of_answered = Flashcard::whereIn('user_answer', ['correct', 'incorrect']);
+
+            // all flashcards to practice
+            $this->table(
+                ["ID", "Question", "User Answer"],
+                $flashcards->toArray()
+            );
+
+            // progress bar
+            $bar = $this->output->createProgressBar($flashcards->count());
+            $bar->advance($count_of_answered->count());
+            $this->newLine(2);
+
+            if ($flashcards->count() === $count_of_answered->count()) {
+                $this->warn('All questions answered. No questions to practice!');
+                goto choice;
+            }
+
+            $id = $this->ask('Which flashcard do you want to practice with? (ID)');
+
+            $flashcard = Flashcard::whereId($id)
+                ->whereIn('user_answer', ['not answered', 'incorrect'])
+                ->first();
+
+            if ($flashcard) {
+                $this->line('Question: ' . $flashcard->question);
+                $user_answer = $this->ask('What is your answer to this question?');
+
+                if ($flashcard->answer === $user_answer) {
+                    $flashcard->user_answer = "correct";
+                    $flashcard->save();
+                    $this->info('Congratulations! You answered the question correctly!');
+                } else {
+                    $flashcard->user_answer = "incorrect";
+                    $flashcard->save();
+                    $this->error('Sorry! Your answer was not correct!');
+                }
+            } else {
+                $this->warn('Sorry! We could not find the flashcard or you have already answered!');
+            }
+
+            if ($this->confirm('Do you wish to continue?')) {
+                goto practice;
+            }
         }
     }
 }
